647ca9e3835c1b16d18e66a750baa2a1521c4ab3
Mayank Chugh <mayank.chugh77@gmail.com>
2020-03-02 23:45:47 +0800

    Second commit after 4th stub

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -4,10 +4,12 @@ import ImageHoster.model.User;
 import ImageHoster.model.UserProfile;
 import ImageHoster.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class UserController {
 
     @Autowired
