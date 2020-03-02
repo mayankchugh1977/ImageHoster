4620063f5e3adab66f5dfb6a8f07c7be58ebb130
Mayank Chugh <mayank.chugh77@gmail.com>
2020-03-02 16:38:49 +0800

    Second commit after 3rd stub

--- src/main/java/ImageHoster/controller/HomeController.java
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -1,20 +1,36 @@
 package ImageHoster.controller;
 
 
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ImageService imageService;
 
     public HomeController(){
-        System.out.println("** HomeController **");
+        System.out.println("** HomeController Constructor **");
     }
 
+    //Write the code to inject the ImageService bean
 
     @RequestMapping("/")
-    public String getRoot(){
+    public String getAllImages(Model model) {
+        //Call getAllImages() method in ImageService class to get the list of all images
+        //Add the list of images in the model with the key as "images"
+
+        List<Image> images = imageService.getAllImages();
+
+        model.addAttribute("images",images);
+
         return "index";
     }
 }
